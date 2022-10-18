2f8539d476c8545a6bcc2b3577faf2d36d1826a6
LouisCeleyronETPA <l.celeyron@ecolescreatives.com>
2022-10-18 12:51:50 +0200

    First Commit

---
+++ Assets/Louis/Scripts/DioBanane.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Julien
+{
+
+    public class DioBanane : MonoBehaviour
+    {
+
+        public Shove shove;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+}
