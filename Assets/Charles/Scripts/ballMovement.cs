38182bba7d6a17aa91fc46b4e841196602a80db5
RENNES\c.blain <c.blain@rns.ecolescreatives.com>
2022-10-31 14:17:59 +0100

    movementtest

--- Assets/Charles/Scripts/ballMovement.cs
+++ Assets/Charles/Scripts/ballMovement.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class ballMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.velocity = rb.velocity + new Vector3(1f,0f,0f);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.velocity = rb.velocity + new Vector3(-1f, 0f, 0f);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb.velocity = rb.velocity + new Vector3(0f, 0f, 1f);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                rb.velocity = rb.velocity + new Vector3(0f, 0f, -1f);
+            }
+        }
+
     }
 }
