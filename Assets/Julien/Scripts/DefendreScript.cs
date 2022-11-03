bf962293fc1aa566177951513f40f9c0338b71f8
JulienBrizou <julienbrizou@gmail.com>
2022-11-03 14:11:17 +0100

    no message

---
+++ Assets/Julien/Scripts/DefendreScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Cunegonde
+{
+    public class DefendreScript : MonoBehaviour
+    {
+        public int chrono = 7600;
+        public float speed = 0.05f;
+        public float vitesseBaguette = 0.0003f;
+        public float sensitivity = 1.5f;
+        public KeyCode avance;
+        public KeyCode recule;
+
+
+        private void OnTriggerExit(Collider other)
+        {
+            Debug.Log("Salut mon pote !");
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.localPosition += transform.up * speed;
+            }
+
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.localPosition += transform.up * -speed;
+            }
+
+
+        }
+        
+    }
+}
