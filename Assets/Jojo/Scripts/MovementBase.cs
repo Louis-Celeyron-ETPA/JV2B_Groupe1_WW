ee2975d58a49a3e1b70f5b72feeb3e060905fe19
RENNES\game <chevalierjolan@gmail.com>
2022-11-03 13:59:41 +0100

    majtamere

--- Assets/Jojo/Scripts/MovementBase.cs
+++ Assets/Jojo/Scripts/MovementBase.cs
@@ -6,44 +6,16 @@ namespace Jojo
 {
     public class MovementBase : MonoBehaviour
     {
-        /*
-        public KeyCode droite;
-        public KeyCode gauche;
-        float speed = 0.13f;
-        */
         public RectTransform rt;
         float speed = 8f;
 
         void Start()
         {
-            {
 
-            }
         }
 
         void Update()
         {
-            /*
-            if (Input.GetKey(droite))
-            {
-                transform.localPosition += transform.right * speed;
-            }
-            else if (Input.GetKey(gauche))
-            {
-                transform.localPosition += transform.right * -speed;
-            }
-
-            if (transform.position.x > 4.24)
-            {
-                transform.position = new Vector3(-3.9f,0.52f,-6.8f);
-                Debug.Log("oui");
-            }
-            if (transform.position.x < -4.24)
-            {
-                transform.position = new Vector3(3.9f, 0.52f, -6.8f);
-                Debug.Log("oui");
-            }
-            */
 
         }
 
@@ -61,6 +33,27 @@ namespace Jojo
 
         public void DownMove(){
             rt.anchoredPosition -= Vector2.up * speed;
+        }
+
+        public void Action()
+        {
+            var positionTemporaire = new Vector3(
+            rt.anchoredPosition.x / Camera.main.pixelWidth,
+            rt.anchoredPosition.y / Camera.main.pixelHeight * -1,
+            Camera.main.nearClipPlane);
+            var convertedPosition = Camera.main.ViewportToWorldPoint(positionTemporaire);
+
+            var positionDansLeMonde = new Vector3(convertedPosition.x, -convertedPosition.y, convertedPosition.z);
+
+            Debug.DrawRay(transform.position, positionDansLeMonde);
+
+            if (Physics.Raycast(positionDansLeMonde, Camera.main.transform.forward, out var other))
+            {
+                if (other.transform.tag == "Voleur")
+                {
+                    Destroy(other.transform.gameObject);
+                }
+            }
         }
 
     }
