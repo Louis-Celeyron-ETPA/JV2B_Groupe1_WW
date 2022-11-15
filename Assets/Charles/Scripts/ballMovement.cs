d08747308c37497d8d5331d6b98c2408d93b5950
RENNES\c.blain <c.blain@rns.ecolescreatives.com>
2022-11-15 15:36:16 +0100

    new ventilo + script movement

--- Assets/Charles/Scripts/ballMovement.cs
+++ Assets/Charles/Scripts/ballMovement.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class ballMovement : MonoBehaviour
 {
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RightMove()
+    {
+        rb.velocity += new Vector3(0.01f, 0f, 0f);
+    }
+    public void LeftMove()
+    {
+        rb.velocity += new Vector3(-0.01f, 0f, 0f);
+    }
+    public void FrontMove()
+    {
+        rb.velocity += new Vector3(0f, 0f, 0.01f);
+    }
+    public void BackMove() 
+    {
+        rb.velocity += new Vector3(0f, 0f, -0.01f);
     }
 }
