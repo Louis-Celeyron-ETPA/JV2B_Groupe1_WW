62e5e917b524e18a369f6220e11ff6b19a8a3152
Paul Leroy <paulo.leroy2@gmail.com>
2022-11-21 14:37:04 +0100

    modèle 3d voiture

--- Assets/Paul/Scripts/MiniJeu3/SpawnBoite.cs
+++ Assets/Paul/Scripts/MiniJeu3/SpawnBoite.cs
@@ -8,21 +8,24 @@ namespace Paul
     {
         public GameObject objectToSpawn;
         public float counter;
+        public int counterMax;
 
         // Start is called before the first frame update
         void Start()
         {
-
+            counterMax = 90;
+            counter = 85;
         }
 
         // Update is called once per frame
         void Update()
         {
             counter++;
-            if (counter > 800f)
+            if (counter > counterMax)
             {
                 Instantiate(objectToSpawn, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
                 counter = 0;
+                counterMax = counterMax - 3;
             }
         }
     }
