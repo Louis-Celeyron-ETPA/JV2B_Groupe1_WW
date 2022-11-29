25c6f0c4489be8f39226e7973e207776b35d0b59
Herbert <l.herbert@rns.ecolescreatives.com>
2022-11-29 15:35:35 +0100

    début Jeu 2

---
+++ Assets/Laura/Scripts/taper.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class taper : MonoBehaviour
+{
+    public Transform initialP;
+    public float baseZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Tape()
+    {
+       
+    }
+}
