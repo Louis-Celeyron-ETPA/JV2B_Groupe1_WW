1feab23466a56f523e0b917b615927c402189d35
Jolan Stgj <94522110+Shokay@users.noreply.github.com>
2023-01-01 03:11:12 +0100

    Bonne année !  J-29 je deviens fou, heureusement que vendredi le ballon est la pour moi Meca MiniJeu3 terminé sinon

--- Assets/Jojo/Scripts/ActionHand.cs
+++ Assets/Jojo/Scripts/ActionHand.cs
@@ -9,16 +9,22 @@ namespace Jojo
     {
         Rigidbody rb;
         public GameObject Hand;
+        public GameObject TasseObject;
         Vector3 PoseServ;
         Vector3 PoseStart;
+        Vector3 PoseReload;
         float PourcentageLerp; 
+        float PourcentageLerp2; 
         bool StartLerp=false;
+        bool InGoBack= false;
         // Start is called before the first frame update
         void Start()
         {
             rb = GetComponent<Rigidbody>();
             PoseServ= new Vector3 (0.180000007f,3.27999997f,-9.14999962f);
+            PoseReload = new Vector3 (0.180000007f,3.27999997f,-11.3999996f);
             PourcentageLerp = 0;
+            PourcentageLerp2 = 0;
             PoseStart = transform.position;
         }
 
@@ -34,7 +40,7 @@ namespace Jojo
             }
             }
 
-            if(StartLerp==false){
+            if(StartLerp==false && TasseObject.activeSelf && InGoBack==false){
                 if(transform.position != PoseStart){
                 Hand.transform.position = Vector3.Lerp(PoseServ,PoseStart,PourcentageLerp);
                 PourcentageLerp += 0.02f;
@@ -43,11 +49,35 @@ namespace Jojo
                 }
             }
             }
+            
+            Debug.Log(PourcentageLerp);
+            
+            if(transform.position != PoseStart && InGoBack==true){
+                Hand.transform.position = Vector3.Lerp(PoseReload,PoseStart,PourcentageLerp);
+                PourcentageLerp += 0.02f;
+                if(PourcentageLerp >= 1){
+                        PourcentageLerp=0;
+                        InGoBack = false;
+                }
+            }
+            
+            if(StartLerp==false && TasseObject.activeSelf == false ){
+                PourcentageLerp=0;
+                if(transform.position != PoseReload){
+                Hand.transform.position = Vector3.Lerp(PoseServ,PoseReload,PourcentageLerp2);
+                PourcentageLerp2 += 0.02f;
+                if(PourcentageLerp2 >= 1){
+                    PourcentageLerp2=0;
+                    TasseObject.gameObject.SetActive(true);
+                    InGoBack = true;
+                }
+            }
+            }
         }
 
         public void ActionServir()
         {
-            if(transform.position == PoseStart){
+            if(Hand.transform.position == PoseStart){
                 StartLerp = true;
             }
         }
