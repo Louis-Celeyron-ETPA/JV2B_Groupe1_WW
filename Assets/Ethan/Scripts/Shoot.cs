3ce9e2e716b1752f5a4a538a9edbe820b4a00027
Ethan Mugnaïni de Ricci <e.mugnainidericci@rns.ecolescreatives.com>
2023-01-10 15:31:03 +0100

    Avancées

--- Assets/Ethan/Scripts/Shoot.cs
+++ Assets/Ethan/Scripts/Shoot.cs
@@ -10,10 +10,7 @@ namespace BananaLover
         public GameObject ballPrefab;
         float force = 20f;
         public GameObject tip;
-
-        // Autres
-        public LineRenderer lr;
-        float rotationSpeed = 0.3f;               
+        float speed = 0.03f;               
 
         // Start is called before the first frame update
         void Start()
@@ -24,9 +21,7 @@ namespace BananaLover
         // Update is called once per frame
         void Update()
         {
-            lr.positionCount = 2;
-            lr.SetPosition(0, transform.position - new Vector3(0, 0.1f, 0)); //Origin
-            lr.SetPosition(1, transform.position + transform.forward * 2);//Origin+direction*longueur
+
         }
 
         public void ShootBullet()
@@ -37,22 +32,22 @@ namespace BananaLover
 
         public void Left()
         {
-            transform.eulerAngles += new Vector3(0, -rotationSpeed, 0);
+            transform.position += new Vector3(-speed, 0, 0);
         }
 
         public void Right()
         {
-            transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+            transform.position += new Vector3(speed, 0, 0);
         }
 
         public void Up()
         {
-            transform.eulerAngles += new Vector3(-rotationSpeed, 0, 0);
+            transform.position += new Vector3(0, speed, 0);
         }
 
         public void Down()
         {
-            transform.eulerAngles += new Vector3(rotationSpeed, 0, 0);
+            transform.position += new Vector3(0, -speed, 0);
         }
     }
 }
